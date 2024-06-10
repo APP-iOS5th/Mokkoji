9d18f642236b6fb24c011d9f5f4864a0e9f7a6ba
jjwon2149 <jjwon2149@gmail.com>
2024-06-10 09:48:15 +0900

    feat: add Model, remove mainStoryboard

---
+++ Mokkoji/Mokkoji/Model/MapInfo.swift
@@ -0,0 +1,14 @@
+//
+//  MapInfo.swift
+//  Mokkoji
+//
+//  Created by 정종원 on 6/10/24.
+//
+
+import Foundation
+
+struct MapInfo: Codable {
+    var placeLatitude: Double
+    var placeLongitude: Double
+    var placeName: String
+}
---
+++ Mokkoji/Mokkoji/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Mokkoji
+//
+//  Created by 정종원 on 6/10/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    var name: String
+    var email: String
+    var profileImageUrl: URL
+    var plan: [Plan]
+    var friendList: [User]?
+}
