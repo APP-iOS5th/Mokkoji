4fa0ec4cfe705cc592d67d157a805e7209db4fea
Jihye Park <deviloper.park@gmail.com>
2024-06-11 13:32:16 +0900

    feat: tableView 추가

--- Mokkoji/Mokkoji/AddPlanViewController.swift
+++ Mokkoji/Mokkoji/AddPlanViewController.swift
@@ -7,14 +7,33 @@
 
 import UIKit
 
-class AddPlanViewController: UIViewController {
-
+class AddPlanViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         
         self.navigationItem.title = "약속 추가"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTapped))
+        
+        self.view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
 
         
     }
@@ -31,6 +50,21 @@ class AddPlanViewController: UIViewController {
         
         /// PlanListView의 title은 inline으로 유지
         self.navigationController?.navigationBar.prefersLargeTitles = false
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO: - 장소 수로 변경
+        5
+        
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO: - 커스텀 셀을 생성하여 장소 리스트에 재사용
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
     }
     
     // MARK: - Methods
