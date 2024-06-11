9633e3baf21d7ab4a1c4adfbae5aac610525aaa9
Jihye Park <deviloper.park@gmail.com>
2024-06-11 18:07:56 +0900

    feat: 필요한 뷰 요소들 추가 & datePicker로 dateField 입력

--- Mokkoji/Mokkoji/AddPlanInfoTableViewCell.swift
+++ Mokkoji/Mokkoji/AddPlanInfoTableViewCell.swift
@@ -21,10 +21,10 @@ class AddPlanInfoTableViewCell: UITableViewCell {
         return datePicker
     }()
     
-    lazy var bodyText: UITextView = {
-        let textView = UITextView()
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        return textView
+    lazy var bodyText: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -35,25 +35,26 @@ class AddPlanInfoTableViewCell: UITableViewCell {
         addSubview(bodyText)
         
         NSLayoutConstraint.activate([
-            titleText.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-            titleText.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-            titleText.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            titleText.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            titleText.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+//            titleText.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            titleText.heightAnchor.constraint(equalToConstant: 50),
             
-            datePicker.topAnchor.constraint(equalTo: titleText.bottomAnchor, constant: 8),
-            datePicker.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-            datePicker.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            datePicker.topAnchor.constraint(equalTo: titleText.bottomAnchor, constant: -8),
+            datePicker.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+//            datePicker.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            datePicker.heightAnchor.constraint(equalToConstant: 30),
             
-            bodyText.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: 8),
-            bodyText.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-            bodyText.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+            bodyText.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: -8),
+            bodyText.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+//            bodyText.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            bodyText.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            bodyText.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: - 뷰에 데이터 연결
-
 
 }
--- Mokkoji/Mokkoji/AddPlanViewController.swift
+++ Mokkoji/Mokkoji/AddPlanViewController.swift
@@ -9,10 +9,87 @@ import UIKit
 
 class AddPlanViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    lazy var titleText: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "약속 제목을 입력하세요."
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var bodyText: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "약속 내용을 간단히 입력하세요."
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var dateField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "약속 날짜를 입력하세요."
+        textField.inputView = datePicker
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .inline
+        datePicker.datePickerMode = .date
+        datePicker.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 400)
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        
+        datePicker.addAction(UIAction { [weak self] _ in
+            self?.dateChanged()
+        }, for: .valueChanged)
+        return datePicker
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    lazy var profileImage: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var inviteButton: UIButton = {
+        let button = UIButton()
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var addMapButton: UIButton = {
+        let button = UIButton()
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var mapView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(AddPlanInfoTableViewCell.self, forCellReuseIdentifier: "addPlanInfoCell")
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -24,18 +101,52 @@ class AddPlanViewController: UIViewController, UITableViewDataSource, UITableVie
         self.view.backgroundColor = .white
         
         self.navigationItem.title = "약속 추가"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTapped))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButtonTapped))
         
+        stackView.addArrangedSubview(profileImage)
+        stackView.addArrangedSubview(inviteButton)
+        
+        self.view.addSubview(titleText)
+        self.view.addSubview(bodyText)
+        self.view.addSubview(dateField)
+        self.view.addSubview(stackView)
+        self.view.addSubview(addMapButton)
+        self.view.addSubview(mapView)
         self.view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            titleText.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleText.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            titleText.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+        
+            bodyText.topAnchor.constraint(equalTo: titleText.bottomAnchor, constant: 5),
+            bodyText.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            bodyText.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            dateField.topAnchor.constraint(equalTo: bodyText.bottomAnchor, constant: 5),
+            dateField.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            dateField.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            stackView.topAnchor.constraint(equalTo: dateField.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            addMapButton.topAnchor.constraint(equalTo: stackView.bottomAnchor),
+            addMapButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            addMapButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            mapView.topAnchor.constraint(equalTo: addMapButton.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            mapView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            mapView.heightAnchor.constraint(equalToConstant: 200),
+            
+            tableView.topAnchor.constraint(equalTo: mapView.bottomAnchor),
             tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
         ])
-
         
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -65,11 +176,23 @@ class AddPlanViewController: UIViewController, UITableViewDataSource, UITableVie
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
         return cell
+        
     }
     
     // MARK: - Methods
-    @objc func doneButtonTapped() {
+    @objc func saveButtonTapped() {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    func dateChanged() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateStyle = .medium
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        let selectedDate = dateFormatter.string(from: datePicker.date)
+        
+        dateField.text = selectedDate
+        dateField.resignFirstResponder()
     }
 
     
