ed5c3974a13a42624ff425370346653adf8a7b15
Jihye Park <deviloper.park@gmail.com>
2024-07-12 17:26:33 +0900

    Ready to Merge

--- Mokkoji/Mokkoji/View/PlanView/AddView/AddPlanViewController.swift
+++ Mokkoji/Mokkoji/View/PlanView/AddView/AddPlanViewController.swift
@@ -411,16 +411,12 @@ class AddPlanViewController: UIViewController, UITableViewDataSource, UITableVie
            let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
             
             let keyboardHeight = keyboardFrame.height
-//            let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
-//            
-//            tableView.contentInset = contentInsets
-//            tableView.scrollIndicatorInsets = contentInsets
             
             /// 현재 선택된 textField가 키보드에 가려지지 않도록 스크롤
             if let activeField = selectedTextField {
-                let activeFieldFrame = activeField.convert(activeField.bounds, to: view)
+                let activeFieldFrame = activeField.convert(activeField.bounds, to: self.view)
                 let activeFieldBottomY = activeFieldFrame.maxY
-                let keyboardOriginY = view.frame.height - keyboardHeight
+                let keyboardOriginY = self.view.frame.height - keyboardHeight
                 
                 /// 키보드의 상단과 텍스트 필드의 하단이 맞닿도록 스크롤
                 if activeFieldBottomY > keyboardOriginY {
