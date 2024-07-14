89176e9f61218c47953667e24ef8895f6e7747f7
Smith <jjwon2149@gmail.com>
2024-07-14 23:16:07 +0900

    feat: 친구 목록 비어있을때 레이블 추가.

--- Mokkoji/Mokkoji/View/ProfileView/FriendListViewController.swift
+++ Mokkoji/Mokkoji/View/ProfileView/FriendListViewController.swift
@@ -7,28 +7,29 @@
 
 import UIKit
 
-class FriendListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+class FriendListViewController: UIViewController{
+        
+    //MARK: - UIComponents
     
-    var allFriends = [
-        User(id:"1236", name: "김홍도", email: "asd@asd.com", profileImageUrl: URL(string: "https://picsum.photos/200/300")!),
-        User(id:"1234", name: "김홍만", email: "asd@asd.com", profileImageUrl: URL(string: "https://picsum.photos/200/300")!),
-        User(id:"1235", name: "김홍기", email: "asd@asd.com", profileImageUrl: URL(string: "https://picsum.photos/200/300")!)
-    ]
-    
-    var filteredFriends = [User]()
-    
-    private lazy var friendSearchBar: UISearchBar = {
-        let searchBar = UISearchBar()
-        searchBar.delegate = self
-        return searchBar
-    }()
-    
+    ///친구목록 테이블
     private lazy var friendSearchTableView: UITableView = {
         let tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(FriendTableViewCell.self, forCellReuseIdentifier: "friendCell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
+    }()
+    
+    //친구목록이 비어있을때 레이블
+    private lazy var isEmptyFriendLabel: UILabel = {
+        let label = UILabel()
+        label.text = "친구목록이 비어있습니다."
+        label.textAlignment = .center
+        label.textColor = .black
+        label.isHidden = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
     override func viewDidLoad() {
@@ -38,22 +39,22 @@ class FriendListViewController: UIViewController, UITableViewDelegate, UITableVi
         self.navigationItem.title = "FRIENDS"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.badge.plus"), style: .plain, target: self, action: #selector(addFriendButtonTapped))
         
-        view.addSubview(friendSearchBar)
         view.addSubview(friendSearchTableView)
-        
-        friendSearchBar.translatesAutoresizingMaskIntoConstraints = false
-        friendSearchTableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(isEmptyFriendLabel)
         
         NSLayoutConstraint.activate([
-            friendSearchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            friendSearchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            friendSearchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            isEmptyFriendLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            isEmptyFriendLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            isEmptyFriendLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             
-            friendSearchTableView.topAnchor.constraint(equalTo: friendSearchBar.bottomAnchor),
+            
+            friendSearchTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             friendSearchTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            friendSearchTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            friendSearchTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            friendSearchTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+        
+        updateEmptyFriendLabel()
+        print("friendList: \(UserInfo.shared.user?.friendList)")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -61,67 +62,44 @@ class FriendListViewController: UIViewController, UITableViewDelegate, UITableVi
         friendSearchTableView.reloadData()
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        if UserInfo.shared.user?.friendList?.first?.name == "친구목록이 비어있습니다." {
-            UserInfo.shared.user?.friendList?.removeFirst()
-        }
-    }
-    
     //MARK: - Methods
-    
     @objc func addFriendButtonTapped() {
-        
+        let addFriendViewController = AddFriendViewController()
+        present(addFriendViewController, animated: true)
     }
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText.isEmpty {
-            filteredFriends.removeAll()
+    private func updateEmptyFriendLabel() {
+        if let friendList = UserInfo.shared.user?.friendList, !friendList.isEmpty {
+            isEmptyFriendLabel.isHidden = true
         } else {
-            filteredFriends = allFriends.filter { $0.name.contains(searchText) }
+            isEmptyFriendLabel.isHidden = false
         }
-        friendSearchTableView.reloadData()
     }
-    
+}
+
+//MARK: - UITableViewDelegate, DataSource Methods
+extension FriendListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredFriends.count
+        return UserInfo.shared.user?.friendList?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath) as! FriendTableViewCell
-        let friend = filteredFriends[indexPath.row]
         
-        cell.userNameLabel.text = friend.name
-        cell.userImageView.load(url: friend.profileImageUrl)
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        let selectedFriend = filteredFriends[indexPath.row]
-        let alertController = UIAlertController(title: "Add Friend", message: "\(selectedFriend.name)을 친구목록에 추가 하시겠습니까?", preferredStyle: .alert)
-        
-        print("\(selectedFriend)")
-        let yesAction = UIAlertAction(title: "Yes", style: .default) { _ in
-            if UserInfo.shared.user!.friendList!.isEmpty {
-                UserInfo.shared.user!.friendList = [selectedFriend]
-            } else {
-                if !UserInfo.shared.user!.friendList!.contains(where: { $0.id == selectedFriend.id }) {
-                    UserInfo.shared.user!.friendList!.append(selectedFriend)
-                } else {
-                    return
-                }
-            }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath)
+        guard let cellImage = UserInfo.shared.user?.friendList?[indexPath.row].profileImageUrl else {
+            return UITableViewCell()
         }
         
-        let noAction = UIAlertAction(title: "No", style: .cancel)
+        cell.textLabel?.text = UserInfo.shared.user?.friendList?[indexPath.row].name
+        cell.imageView?.image = UIImage(systemName: "person.circle")
+        cell.imageView?.load(url: cellImage)
         
-        alertController.addAction(yesAction)
-        alertController.addAction(noAction)
+        let imageSize: CGFloat = 50
+        cell.imageView?.frame = CGRect(x: 0, y: 0, width: imageSize, height: imageSize)
+        cell.imageView?.layer.cornerRadius = imageSize / 3.6
+        cell.imageView?.clipsToBounds = true
+        cell.selectionStyle = .none
         
-        present(alertController, animated: true, completion: nil)
+        return cell
     }
-    
 }
-
